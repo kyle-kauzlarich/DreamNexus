b820d72f6386e7eff63b24ce7ea5bebf4b7e46cd
kyle-kauzlarich <kauzlarich.kyle@gmail.com>
2018-05-15 20:31:26 -0700

    Change log added img/dreamnexus.png -- unused as of now
    app.js added a rough nav bar and baner and space designated for the
    bottom half of the page/app to go
    
    app.css added styles to help get the nav and baner get closer to how the
    rest of dream nexus looks -- using a lazy implamention of the BEM naming
    convention atm
    
    index.css added in a css reset

--- colabs/react/src/App.js
+++ colabs/react/src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <a href="http://thelizmckenzie.com/dreamnexus/">Take Me Home</a>
+      <div className='app-bkg'>
+        <div className="app__container">
+
+          <div className='app__container-top'>
+
+            <div className='app__container-nav'>
+              <span className='container-nav-item'>
+                <a href="http://thelizmckenzie.com/dreamnexus/"
+                >Home</a>
+              </span>
+              <span className='container-nav-item'>
+              <a href="https://lambdaschoolstudents.slack.com"
+              >Slack</a>
+              </span>
+              <span className='container-nav-item'>Google goes here</span>
+              <span className='container-nav-item'>
+              <a href='#'
+              >Calendar</a>
+              </span>
+              <span className='container-nav-item'>
+              <a href="./colabs/index.html"
+              >Colabs</a>
+              </span>
+            {/* <a href="http://thelizmckenzie.com/dreamnexus/"
+            >Take Me Home</a> */}
+            </div> {/* app container nav end */}
+            <img src='http://thelizmckenzie.com/dreamnexus/DreamNexus/img/dreamnexus.png'
+            className='app__container-img' 
+            alt="baner logo" />
+
+          </div> {/* app container top end */}
+          <div className='app__container-bottom'>
+          
+          </div> {/* app container bottom end */}
+        </div> {/* app container end */}
       </div>
     );
   }
